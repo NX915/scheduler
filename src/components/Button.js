06412962cdfda3b154a2286e5b90b4e004a967d9
Xiang Ni <XiangNi@Xiangs-MacBook-Pro.local>
2020-09-28 16:47:37 -0400

    add w6 d1 code

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,20 @@ import React from "react";
 
 import "components/Button.scss";
 
+const classnames = require('classnames');
+
+
 export default function Button(props) {
-   return <></>;
+
+   return <button 
+   className={
+      classnames('button', {
+         'button--confirm':props.confirm,
+         'button--danger':props.danger,
+      })
+   }
+   disabled={props.disabled}
+   onClick={props.onClick}>
+      {props.children}
+   </button>;
 }
