b6e4a1f4f905e94a8a5681cb9f7705b430bfe364
Xiang Ni <XiangNi@Xiangs-MacBook-Pro.local>
2020-09-28 20:28:33 -0400

    complete day list component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,6 +1,20 @@
 import React from "react";
 
+import DayListItem from "components/DayListItem";
+
 export default function DayList(props) {
 
-  return (<ul></ul>);
+  return (
+  <ul>
+    {
+      props.days.map((day) => {
+      return <DayListItem 
+      name={day.name}
+      spots={day.spots}
+      setDay={props.setDay}
+      selected={day.name === props.day}/>
+      })
+    }
+  </ul>
+  );
 }
