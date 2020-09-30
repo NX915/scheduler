9daea90c5d04c2c321e1db79d4905762cd9bb350
Xiang Ni <XiangNi@Xiangs-MacBook-Pro.local>
2020-09-30 14:59:13 -0400

    add network request to days component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,28 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 
 import DayList from  "components/DayList";
 import Appointments from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -84,6 +85,16 @@ const appointments = [
 
 export default function Application(props) {
   const [ day, setDay ] = useState("Monday");
+  const [ days, setDays ] = useState([]);
+
+  useEffect(() => {
+      axios.get('/api/days')
+        .then(res => {
+          // console.log(res);
+          setDays([...res.data]);
+        });
+  }, []);
+
 
   return (
     <main className="layout">
