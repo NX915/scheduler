4bdd77d0cc02d3a254504d6e3d36f421f30ecc82
Xiang Ni <XiangNi@Xiangs-MacBook-Pro.local>
2020-09-30 22:26:34 -0400

    add custom hooks for appointment

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function(newMode) {
+    setMode(newMode);
+    setHistory(prev => {
+      // console.log(prev);
+      const newArr = [...prev];
+      newArr.push(newMode);
+      // console.log(newArr);
+      return newArr;
+    });
+  };
+
+  const back = function() {
+    setHistory(prev => {
+      // console.log(prev);
+      const newArr = [...prev];
+      if (newArr.length > 1) {
+        newArr.pop();
+      }
+      // console.log(newArr);
+      return newArr;
+    });
+    // console.log(history[history.length - 1]);
+    
+    const backAmount = history.length > 1 ? 2 : 1;
+    setMode(history[history.length - backAmount]);
+  };
+
+  return {mode, transition, back};
+};
